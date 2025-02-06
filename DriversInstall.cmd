fec3e7ba2ad62516ea1a57ff389b338e600cba6f
GorstakZadar <znastidobrostoje@gmail.com>
2025-02-06 08:37:03 +0100

    Add files via upload

---
+++ DriversInstall.cmd
@@ -0,0 +1,8 @@
+﻿@echo off
+
+:: Elevate
+>nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
+DEL /F /Q "%temp%\uac.vbs"
+
+:: Drivers Install
+pnputil.exe /add-driver *.inf /subdirs /install
